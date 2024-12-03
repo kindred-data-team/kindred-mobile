262fa224d64787d7b29e44cd64eaf4db9f91b83b
ralphkindred <ralph@mykindred.co>
2024-12-04 02:04:25 +0800

    add session

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:kindred_app/core/presentation/navigation/app_router.dart';
+import 'package:kindred_app/core/services/session_service.dart';
 import 'package:kindred_app/features/app/presentation/views/app.dart';
 
 Future<void> main() async {
+  await Session().init();
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(App(
